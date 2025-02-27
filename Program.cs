e54fffb26dd10f28172faf719d00188e78b36c83
Arsany002 <134439681+Arsany002@users.noreply.github.com>
2025-02-27 19:58:48 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to islam's carpet cleaning center");
+            Console.WriteLine("Enter the number of small carpets");
+            int smallnumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number of large carpets");
+            int largenumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("price of small carpets is 25 $\n");
+            Console.WriteLine("price of large carpets is 35 $\n"); 
+            int total = (smallnumber * 25) + (largenumber * 35);
+            double tax = total * 0.6;
+            Console.WriteLine($"the totoal amount is {total}$\n");
+            Console.WriteLine($"the tax applied is {tax}$\n");
+            Console.WriteLine("==========================================");
+            double totalestimate = total + tax;
+            Console.WriteLine($"The total estimate is {totalestimate} $ ");
+            Console.WriteLine("This estimate is available only for 30 days");
+
+
+        }
+    }
+}
