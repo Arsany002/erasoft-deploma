95aee529474b7da2e9930bef7a3212e8d8b00387
Arsany002 <134439681+Arsany002@users.noreply.github.com>
2025-02-27 20:38:34 +0200

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,9 @@
+﻿int X = 10;
+int Y = 20;
+Console.WriteLine($"Equation: {X} + {Y} = {X + Y:C}");
+//1-the output is $30.00 because we used the currency specifier (c)
+//2-we can benfit from it to make out codes more specific rather than some random numbers
+// 3-
+int x = 10;
+int y = 20;
+Console.WriteLine($"Equation : {x} + {y} = {x + y:f2}");
